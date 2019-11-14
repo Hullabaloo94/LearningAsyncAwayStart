0c99316e977432e675573907f91cd6d1629df0e6
Jason Hall <jasonahall1994@gmail.com>
2019-11-14 12:19:37 +0000

    initial commit

---
+++ LearningAsyncAwayStart/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAsyncAwayStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("downloading file");
+            Download();
+            Console.ReadLine();
+
+        }
+
+
+        /**
+         * Download is an async function which means that it will allow for the main program to continue down its program sequence whilst it completes what is within the Download function.
+         * However, within the Download function is an await clause. This clause makes the function wait until it has completed its specified task after the await clause.
+         */
+        static async void Download()
+        {
+            HttpClient client = new HttpClient();
+            var data = await client.GetStringAsync("http://rouxacademy.com");
+            Console.WriteLine("Download Complete\n" + data);
+        }
+    }
+
+
+}
